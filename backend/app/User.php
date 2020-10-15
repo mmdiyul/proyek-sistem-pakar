209922bc38dfed831dbc15316043d073d5536d4c
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2020-10-15 23:05:28 +0700

    add user model

---
+++ backend/app/User.php
@@ -0,0 +1,43 @@
+<?php
+namespace App;
+
+use App\Roles;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role_id',
+        'fullname',
+        'username',
+        'email',
+        'password',
+        'last_logged_in'
+    ];
+
+    /**
+     * The attributes that are hidden.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password'
+    ];
+
+    public function role()
+    {
+        return $this->belongsTo(Roles::class, 'role_id');
+    }
+}
